de99d134b682d04399d45e4de8f624f84d23eb31
Minh Pham <phamminh91@gmail.com>
2015-07-27 21:05:32 +0800

    Change deploy script

--- deploy/setup-deploy.sh
+++ deploy/setup-deploy.sh
@@ -3,15 +3,17 @@ wget https://bootstrap.pypa.io/get-pip.py
 
 
 rm -f /etc/nginx/sites-available/vonvon/*.conf
+rm -f /etc/nginx/sites-enabled/vonvon/*.conf
+
 
 # Copy nginx configuration files
 mkdir -p /etc/nginx/sites-available/vonvon
 mkdir -p /etc/nginx/sites-enabled/vonvon
 cp -r nginx/*.conf /etc/nginx/sites-available/vonvon
 cp -r nginx/*.conf /etc/nginx/sites-enabled/vonvon
-ln -s /etc/nginx/sites-available/vonvon/client.conf /etc/nginx/sites-enabled/vonvon/client.conf
-ln -s /etc/nginx/sites-available/vonvon/wsgi.conf /etc/nginx/sites-enabled/vonvon/wsgi.conf
-ln -s /etc/nginx/sites-available/vonvon/peer.conf /etc/nginx/sites-enabled/vonvon/peer.conf
+ln -sf /etc/nginx/sites-available/vonvon/client.conf /etc/nginx/sites-enabled/vonvon/client.conf
+ln -sf /etc/nginx/sites-available/vonvon/wsgi.conf /etc/nginx/sites-enabled/vonvon/wsgi.conf
+ln -sf /etc/nginx/sites-available/vonvon/peer.conf /etc/nginx/sites-enabled/vonvon/peer.conf
 
 mkdir -p ~/nodejs/log
 
