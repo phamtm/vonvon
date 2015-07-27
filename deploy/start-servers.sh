7f058cfc6f09927d02f73c1ee797149cc80f7b18
Minh Pham <phamminh91@gmail.com>
2015-07-27 21:08:56 +0800

    Change server process to run in background mode

--- deploy/start-servers.sh
+++ deploy/start-servers.sh
@@ -1,12 +1,12 @@
 cd ~/nodejs/daemon
-python daemon.py >> ~/nodejs/daemon.log
+python daemon.py >> ~/nodejs/daemon.log &
 
 # Redis
-redis-server >> ~/nodejs/redis-server.log
+redis-server >> ~/nodejs/redis-server.log &
 
 # Client - 8000
 cd ~/nodejs/client
-python -m SimpleHTTPServer >> ~/nodejs/client.log
+python -m SimpleHTTPServer >> ~/nodejs/client.log &
 
 
 # Peer server - 8001
@@ -16,4 +16,7 @@ peerjs --port 8000 --key peerjs &
 
 # Server - 8002
 cd ~/nodejs/wsgi
-node server.js >> ~/nodejs/wsgi.log
+node server.js >> ~/nodejs/wsgi.log &
+
+
+service nginx restart
