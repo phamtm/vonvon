f00cbed3e1c1e6af143433b54043cedcbdec7eb5
Minh Pham <phamminh91@gmail.com>
2015-07-27 21:10:38 +0800

    Change log location

--- deploy/start-servers.sh
+++ deploy/start-servers.sh
@@ -1,22 +1,23 @@
 cd ~/nodejs/daemon
-python daemon.py >> ~/nodejs/daemon.log &
+python daemon.py >> ~/nodejs/log/daemon.log &
+
 
 # Redis
-redis-server >> ~/nodejs/redis-server.log &
+redis-server >> ~/nodejs/log/redis-server.log &
 
 # Client - 8000
 cd ~/nodejs/client
-python -m SimpleHTTPServer >> ~/nodejs/client.log &
+python -m SimpleHTTPServer >> ~/nodejs/log/client.log &
 
 
 # Peer server - 8001
 cd ~/peerjs-server
-peerjs --port 8000 --key peerjs &
+peerjs --port 8000 --key peerjs >> ~/nodejs/log/peerjs.log &
 
 
 # Server - 8002
 cd ~/nodejs/wsgi
-node server.js >> ~/nodejs/wsgi.log &
+node server.js >> ~/nodejs/log/wsgi.log &
 
 
 service nginx restart
