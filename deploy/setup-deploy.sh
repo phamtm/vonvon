031ddc09a87b847dc8dd1ef91071d8d46a56122a
Minh Pham <phamminh91@gmail.com>
2015-07-27 21:18:11 +0800

    Add message to setup script

--- deploy/setup-deploy.sh
+++ deploy/setup-deploy.sh
@@ -1,7 +1,13 @@
-apt-get install redis nodejs
+echo 'Installing redis, nodejs, nginx..'
+apt-get install redis nodejs nginx
+
+echo 'Installing pip..'
 wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+rm -f get-pip.py
 
 
+echo 'Setting up nginx config..'
 rm -f /etc/nginx/sites-available/vonvon/*.conf
 rm -f /etc/nginx/sites-enabled/vonvon/*.conf
 
@@ -15,9 +21,12 @@ ln -sf /etc/nginx/sites-available/vonvon/client.conf /etc/nginx/sites-enabled/vo
 ln -sf /etc/nginx/sites-available/vonvon/wsgi.conf /etc/nginx/sites-enabled/vonvon/wsgi.conf
 ln -sf /etc/nginx/sites-available/vonvon/peer.conf /etc/nginx/sites-enabled/vonvon/peer.conf
 
+
+echo 'Creating log folder..'
 mkdir -p ~/nodejs/log
 
 
 # Setup nodejs packages
+echo 'Setting up node packages..'
 cd ~/nodejs/wsgi
 npm install
