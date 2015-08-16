f3f4c0aad4191fcf1875ec001ca4114fb864a97c
Minh Pham <phamminh91@gmail.com>
2015-08-16 17:19:25 +0800

    Add modernizr feature detection

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,19 @@
 var App = require('./components/App.jsx');
 var React = require('react');
 
+require('browsernizr/test/webrtc/getusermedia');
+require('browsernizr/test/webrtc/peerconnection');
+// make sure to do this _after_ importing the tests
+// or if you need access to the modernizr instance:
+var Modernizr = require('browsernizr');
 
-React.render(
-  <App />,
-  document.getElementById('appComponent')
-);
+if (!Modernizr.getusermedia && !Modernizr.peerconnection) {
+  window.location.replace('not-supported.html');
+} else {
+  React.render(
+    <App />,
+    document.getElementById('appComponent')
+  );
+}
+
+
