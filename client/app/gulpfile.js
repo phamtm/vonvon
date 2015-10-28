88760c711f845cd3063cdf7107dea8c27c28e4d7
Minh Pham <phamminh91@gmail.com>
2015-10-28 22:02:39 +0800

    Add build and dist script. Remove redundant static files

--- client/app/gulpfile.js
+++ client/app/gulpfile.js
@@ -6,18 +6,19 @@ const minifyCss = require('gulp-minify-css');
 const reactify = require('reactify');
 const rename = require('gulp-rename');
 const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 const streamify = require('gulp-streamify');
 const uglify = require('gulp-uglify');
 const less = require('gulp-less');
 
 
 // Concatenate and minify CSS
-gulp.task('css', function() {
-  // return gulp.src('./less/**/style.less')
-  //             .pipe(less({
-  //              paths: [ path.join(__dirname, 'less') ]
-  //             }))
-  //             .pipe(gulp.dest('./css'));
+gulp.task('less', function() {
+  return gulp.src('../ui2/less/style.less')
+             .pipe(less( { paths: [ '../ui2/less' ] }))
+             .pipe(minifyCss())
+             .pipe(gulp.dest('./dist/css'))
+             .pipe(gulp.dest('../ui2/css'));
 });
 
 // Browserify
@@ -27,29 +28,50 @@ gulp.task('js', function () {
         transform: [reactify]
       }).bundle();
 
-  return bundleStream
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./dist/js'));
+  return bundleStream.pipe(source('bundle.js'))
+                     .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('js-compress', function () {
+  const bundleStream = browserify('./js/app.jsx', {
+        debug: true,
+        transform: [reactify]
+      }).bundle();
+
+  return bundleStream.pipe(source('bundle.js'))
+                     .pipe(buffer())
+                     .pipe(uglify())
+                     .pipe(gulp.dest('./dist/js'));
 });
 
 // Copy HTML
 gulp.task('html', function () {
   return gulp.src('./static/**/*.html')
-  .pipe(gulp.dest('./dist'));
+             .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('images', function() {
+  return gulp.src('../ui2/img/**/*')
+             .pipe(gulp.dest('./dist/img/'));
+});
+
+gulp.task('static', ['html', 'images']);
+
+
+/* Build for release */
+gulp.task('dist', ['js-compress', 'less', 'static']);
+
+/* Build for dev task */
 gulp.task('build', function () {
-  gulp.start(['js', 'html']);
+  gulp.start(['js', 'static', 'less']);
 });
 
-gulp.task('dist', function() {
-  // TODO: build, compress js and copy html
-});
-
+/* Continuous build task */
 gulp.task('watch', ['build'], function () {
   gulp.watch(['./js/**/*.js', './js/**/*.jsx'], ['js']);
   gulp.watch(['./static/**/*.html'], ['html']);
-  gulp.watch(['../ui2/less/**/*.less', '../ui2/less/**/*.css'], ['css']);
+  gulp.watch(['../ui2/less/**/*'], ['less']);
 });
 
+/* Default to continuous build */
 gulp.task('default', ['watch']);
